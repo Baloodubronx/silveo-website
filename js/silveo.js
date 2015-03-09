dcd4967cbb0eb478fa79bff5b450ea5a24504b66
baloodubronx <julien.autebert@gmail.com>
2015-03-09 16:34:03 +0100

    first draft of animations

--- js/silveo.js
+++ js/silveo.js
@@ -1,24 +1,126 @@
-$(document).on('scroll', function(){
-  var logo = $('.logo');
-  var position = $('body').scrollTop();
-  var degrees = $('body').scrollTop()/3; // Get position of the body
+var section = 0; // Active section
+var windowHeight = 0; // The window height (lets hope they don't resize)
+var windowWidth = 0;  // The window width
+var curpos=0;
+var animate=false;
 
-  logo.css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
-                 '-moz-transform' : 'rotate('+ degrees +'deg)',
-                 '-ms-transform' : 'rotate('+ degrees +'deg)',
-                 'transform' : 'rotate('+ degrees +'deg)'});
-
-  console.log(position);
-
-  if (position < 0) {
-    $('body').scrollTop(0);
-  }
-
-  if (position < 850) {
-    $('.logocontainer').removeClass('fixed');
-  }
-
-  if (position> 850) {
-    $('.logocontainer').addClass('fixed');
-  }
+// SCROLL BACK TO TOP BEFORE UNLOADING PAGE -> ENSURE ON TOP AT EVERY LOADING
+$(window).on('beforeunload', function(){
+  $(this).scrollTop(0);
 });
+
+// WHEN DOM IS READY
+$(document).ready(function() {
+  windowHeight = $(window).height();
+  windowWidth = $(window).width();
+  console.log(windowHeight);
+  // Scroll back to top (just making sure)
+  $(this).scrollTop(0);
+
+
+  TweenLite.from("#logo", 1, {opacity:0, delay:0.1});
+  TweenLite.from("#title1", 1.5, {opacity:0, delay:1});
+  TweenLite.from("#title2", 1, {opacity:0, delay:2});
+  TweenLite.from("#bg1", 2, {opacity:0, delay:3});
+  TweenLite.from(".scrolldown", 2, {opacity:0, delay:3});
+
+  TweenLite.to("#cont1", 0.1, {marginLeft:2*windowWidth+'px'});
+  TweenLite.to("#cont2", 0.1, {marginRight:2*windowWidth+'px'});
+  TweenLite.from(".one-long-bar", 1, {opacity:0, delay:4.5});
+
+  $('body').css({'overflow-y':'hidden'});
+
+  setTimeout(function(){
+    $('body').css({'overflow-y':'visible'});
+    $('body').scrollTop(0);
+  }, 4000);
+
+});
+
+
+
+$(document).on('scroll', function(){
+  var position = $('body').scrollTop();
+  console.log(position);
+  // MAKE THE CURRENT BG PARALLALALALALALAXABLE
+  parallax(position);
+
+  // NO SECTION
+  if (position < windowHeight*3/5) {
+    TweenLite.to("#h11", 1, {opacity:0});
+    TweenLite.to("#roundy1", 1, {opacity:0});
+    TweenLite.to("#cont1", 1, {marginLeft:2*windowWidth+'px'});
+    TweenLite.to("#cont2", 1, {marginRight:2*windowWidth+'px'});
+    TweenLite.to("#cont3", 0.01, {padding: 0});
+    TweenLite.to("#cont3", 0.8, {height:0});
+  }
+
+  // SECTION 1
+  if ((position > windowHeight*3/5) && (position < 4/5*windowHeight)) {
+    if (section!==1) {
+      TweenLite.to("#h11", 1, {opacity:1});
+      section =1;
+
+      // remove
+      TweenLite.to("#roundy1", 1, {opacity:0});
+      TweenLite.to("#cont1", 1, {marginLeft:2*windowWidth+'px'});
+      TweenLite.to("#cont2", 1, {marginRight:2*windowWidth+'px'});
+      TweenLite.to("#cont3", 0.01, {padding: 0});
+      TweenLite.to("#cont3", 0.8, {height:0});
+    }
+  }
+
+  // SECTION 2
+  if ((position > windowHeight*4/5) && (position < windowHeight)) {
+    if (section !== 2){
+      TweenLite.to("#roundy1", 1, {opacity:1});
+      TweenLite.to("#h11", 1, {opacity:1});
+      section = 2;
+
+      TweenLite.to("#cont1", 1, {marginLeft:2*windowWidth+'px'});
+      TweenLite.to("#cont2", 1, {marginRight:2*windowWidth+'px'});
+      TweenLite.to("#cont3", 0.01, {padding: 0});
+      TweenLite.to("#cont3", 0.8, {height:0});
+    }
+  }
+
+  // SECTION 3
+  if ((position > windowHeight) && (position < windowHeight+200)) {
+    if (section !== 3){
+      $('.mimik').css({'height': '70px'});
+      $('#nav').addClass('fixed');
+      TweenLite.to("#cont1", 1, {marginLeft:'50%'});
+      section = 3;
+
+      TweenLite.to("#cont2", 1, {marginRight:2*windowWidth+'px'});
+      TweenLite.to("#cont3", 0.01, {padding: 0});
+      TweenLite.to("#cont3", 0.8, {height:0});
+    }
+  }
+
+  // SECTION 4
+  if ((position>windowHeight+200) && (position < windowHeight*3/2)) {
+    if (section !== 4) {
+      section = 4;
+      TweenLite.to("#cont2", 1, {marginRight:'50%'});
+
+      TweenLite.to("#cont3", 0.01, {padding: 0});
+      TweenLite.to("#cont3", 0.8, {height:0});
+    }
+  }
+
+  // SECTION 5
+  if ((position>windowHeight*1.9) ) {
+    if (section !== 5) {
+      section = 5;
+      TweenLite.to("#cont3", 0.01, {padding: '40px 20px'});
+      TweenLite.to("#cont3", 0.8, {height:'200px'});
+    }
+  }
+
+});
+
+function parallax(scrolled) {
+  var coeff=-0.2;
+  $('.one-long-bar').css('background-position', '50% '+ scrolled*coeff+'px');
+}
---
+++ js/silveo2.js
@@ -0,0 +1,102 @@
+// GLOBAL VARIABLES
+var section = 1; // Active section
+var windowHeight = 0; // The window height (lets hope they don't resize)
+var windowWidth = 0;  // The window width
+
+
+// SCROLL BACK TO TOP BEFORE UNLOADING PAGE -> ENSURE ON TOP AT EVERY LOADING
+$(window).on('beforeunload', function(){
+  $(this).scrollTop(0);
+});
+
+// WHEN DOM IS READY
+$(document).ready(function() {
+  windowHeight = $(window).height();
+  windowWidth = $(window).width();
+
+  // Scroll back to top (just making sure)
+  $(this).scrollTop(0);
+
+  // Background should have the window min-height
+  $('.section-container').css({'height' : windowHeight});
+  $('.one-bg').css({'height' : 1.2*windowHeight});
+
+
+  TweenLite.from("#logo", 1, {opacity:0, delay:0.1});
+  TweenLite.from("#title-one", 1.5, {opacity:0, delay:1});
+  TweenLite.from("#title-two", 1, {opacity:0, fontSize:10, delay:2});
+  TweenLite.from("#bg-one", 2, {opacity:0, delay:3});
+
+  //$('#logo').delay(100).animate({'opacity':1}, 1000);
+  //$('#title-one').delay(1000).animate({'opacity':1}, 1500);
+  //$('#title-two').delay(3000).animate({'opacity':1}, 1000);
+  //$('#bg-one').delay(4000).animate({'opacity':1}, 1000);
+
+  $('body').css({'overflow':'hidden'});
+
+  setTimeout(function(){
+    $('body').css({'overflow':'visible'});
+    $('body').scrollTop(0);
+  }, 4000);
+
+});
+
+
+
+$(document).on('scroll', function(){
+  var position = $('body').scrollTop();
+  console.log(position);
+
+  // MAKE THE CURRENT BG PARALLALALALALALAXABLE
+  parallax(position);
+
+
+  if (position < windowHeight) {
+    $('.mimik').css({'height': '0px'});
+    $('#navbar').removeClass('fixed');
+    TweenLite.to("#navbar", 0.3, {opacity:0});
+  }
+  else {
+    $('.mimik').css({'height': '80px'});
+    $('#navbar').addClass('fixed');
+    TweenLite.to("#navbar", 0.3, {opacity:1});
+  }
+
+  if (position>windowHeight && position<2*windowHeight) {
+    $('#info-one').animate({'opacity':1}, 1000);
+    $('#info-two').animate({'opacity':1}, 1000);
+    $('#info-three').animate({'opacity':1}, 1000);
+  }
+
+
+});
+
+
+function parallax(scrolled) {
+  var height = windowHeight;
+  if (scrolled<windowHeight) {
+    if (section!==1) {
+      section=1;
+      $('#bg-one').css({'background':'url(images/airport.png) no-repeat center'});
+      $('#bg-one').css({'background-size':'100%'});
+    }
+    $('#bg-one').css('top', -(scrolled*0.2)+'px');
+  }
+  if (scrolled>windowHeight && scrolled<2*windowHeight) {
+    if (section!==2) {
+      section=2;
+      $('#bg-two').css({'background':'url(images/2.png) no-repeat center'});
+      $('#bg-two').css({'background-size':'120%'});
+    }
+    $('#bg-two').css('top', -((scrolled-windowHeight)*0.2)+'px');
+  }
+  if (scrolled>2*windowHeight && scrolled<3*windowHeight) {
+    if (section!==3) {
+      section=3;
+    }
+    $('#bg-two').css('top', -((scrolled-windowHeight)*0.2)+'px');
+  }
+  if (scrolled>3*windowHeight && scrolled<4*windowHeight) {
+    $('#bg-one').css('top', -((scrolled-4*windowHeight)*0.2)+'px');
+  }
+}
