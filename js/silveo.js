18b1cb2dc907097f7b0d320158e3717dd6bf6974
baloodubronx <julien.autebert@gmail.com>
2015-03-09 22:27:23 +0100

    the function

--- js/silveo.js
+++ js/silveo.js
@@ -3,6 +3,9 @@ var windowHeight = 0; // The window height (lets hope they don't resize)
 var windowWidth = 0;  // The window width
 var curpos=0;
 var animate=false;
+var section_start = [];
+// DEFINE THE SECTIONS
+
 
 // SCROLL BACK TO TOP BEFORE UNLOADING PAGE -> ENSURE ON TOP AT EVERY LOADING
 $(window).on('beforeunload', function(){
@@ -12,8 +15,13 @@ $(window).on('beforeunload', function(){
 // WHEN DOM IS READY
 $(document).ready(function() {
   windowHeight = $(window).height();
+  var wh = windowHeight;
   windowWidth = $(window).width();
-  console.log(windowHeight);
+
+  section_start = [0, wh*3/5, wh*4/5, wh, wh+300, wh+650, wh+950];
+
+
+  //console.log(windowHeight);
   // Scroll back to top (just making sure)
   $(this).scrollTop(0);
 
@@ -28,7 +36,7 @@ $(document).ready(function() {
   TweenLite.to("#cont2", 0.1, {marginRight:2*windowWidth+'px'});
   TweenLite.from(".one-long-bar", 1, {opacity:0, delay:4.5});
 
-  $('body').css({'overflow-y':'hidden'});
+  //$('body').css({'overflow-y':'hidden'});
 
   setTimeout(function(){
     $('body').css({'overflow-y':'visible'});
@@ -41,80 +49,24 @@ $(document).ready(function() {
 
 $(document).on('scroll', function(){
   var position = $('body').scrollTop();
-  console.log(position);
+  //console.log(position);
   // MAKE THE CURRENT BG PARALLALALALALALAXABLE
   parallax(position);
 
-  // NO SECTION
-  if (position < windowHeight*3/5) {
-    TweenLite.to("#h11", 1, {opacity:0});
-    TweenLite.to("#roundy1", 1, {opacity:0});
-    TweenLite.to("#cont1", 1, {marginLeft:2*windowWidth+'px'});
-    TweenLite.to("#cont2", 1, {marginRight:2*windowWidth+'px'});
-    TweenLite.to("#cont3", 0.01, {padding: 0});
-    TweenLite.to("#cont3", 0.8, {height:0});
-  }
-
-  // SECTION 1
-  if ((position > windowHeight*3/5) && (position < 4/5*windowHeight)) {
-    if (section!==1) {
-      TweenLite.to("#h11", 1, {opacity:1});
-      section =1;
-
-      // remove
-      TweenLite.to("#roundy1", 1, {opacity:0});
-      TweenLite.to("#cont1", 1, {marginLeft:2*windowWidth+'px'});
-      TweenLite.to("#cont2", 1, {marginRight:2*windowWidth+'px'});
-      TweenLite.to("#cont3", 0.01, {padding: 0});
-      TweenLite.to("#cont3", 0.8, {height:0});
-    }
-  }
-
-  // SECTION 2
-  if ((position > windowHeight*4/5) && (position < windowHeight)) {
-    if (section !== 2){
-      TweenLite.to("#roundy1", 1, {opacity:1});
-      TweenLite.to("#h11", 1, {opacity:1});
-      section = 2;
-
-      TweenLite.to("#cont1", 1, {marginLeft:2*windowWidth+'px'});
-      TweenLite.to("#cont2", 1, {marginRight:2*windowWidth+'px'});
-      TweenLite.to("#cont3", 0.01, {padding: 0});
-      TweenLite.to("#cont3", 0.8, {height:0});
-    }
-  }
-
-  // SECTION 3
-  if ((position > windowHeight) && (position < windowHeight+200)) {
-    if (section !== 3){
-      $('.mimik').css({'height': '70px'});
-      $('#nav').addClass('fixed');
-      TweenLite.to("#cont1", 1, {marginLeft:'50%'});
-      section = 3;
-
-      TweenLite.to("#cont2", 1, {marginRight:2*windowWidth+'px'});
-      TweenLite.to("#cont3", 0.01, {padding: 0});
-      TweenLite.to("#cont3", 0.8, {height:0});
-    }
-  }
-
-  // SECTION 4
-  if ((position>windowHeight+200) && (position < windowHeight*3/2)) {
-    if (section !== 4) {
-      section = 4;
-      TweenLite.to("#cont2", 1, {marginRight:'50%'});
-
-      TweenLite.to("#cont3", 0.01, {padding: 0});
-      TweenLite.to("#cont3", 0.8, {height:0});
-    }
-  }
-
-  // SECTION 5
-  if ((position>windowHeight*1.9) ) {
-    if (section !== 5) {
-      section = 5;
-      TweenLite.to("#cont3", 0.01, {padding: '40px 20px'});
-      TweenLite.to("#cont3", 0.8, {height:'200px'});
+  for (var i=0; i<section_start.length; i++) {
+    var top = section_start[i];
+    var bottom = section_start[i+1] || 10000;
+    //console.log(top + ' - ' + position + ' - ' + bottom);
+    if ( (top < position) && (position < bottom)) {
+      if (section !== i) {
+        section = i;
+        for (var j=0; j<=i; j++) {
+          sectionsEnter[j]();
+        }
+        for (var k=i+1; k<section_start.length; k++) {
+          sectionsLeave[k]();
+        }
+      }
     }
   }
 
@@ -124,3 +76,81 @@ function parallax(scrolled) {
   var coeff=-0.2;
   $('.one-long-bar').css('background-position', '50% '+ scrolled*coeff+'px');
 }
+
+var sectionsEnter = [
+  // SECTION 0
+  function () {
+    console.log('entering section 0');
+  },
+
+  // SECTION 1
+  function () {
+    console.log('entering section 1');
+    TweenLite.to("#h11", 1, {opacity:1});
+  },
+  // SECTION 2
+  function() {
+    console.log('entering section 2');
+    TweenLite.to("#roundy1", 1, {opacity:1});
+  },
+  // SECTION 3
+  function() {
+    console.log('entering section 3');
+    $('.mimik').css({'height': '70px'});
+    $('#nav').addClass('fixed');
+    TweenLite.to("#cont1", 1, {marginLeft:'50%'});
+  },
+  // SECTION 4
+  function() {
+    console.log('entering section 4');
+    TweenLite.to("#cont2", 1, {marginRight:'50%'});
+  },
+  // SECTION 5
+  function() {
+    console.log('entering section 5');
+    TweenLite.to("#cont3", 0.4, {opacity:1});
+  },
+  // SECTION 6
+  function() {
+    TweenLite.to("#cont4", 1, {height:400, opacity:1});
+    console.log('entering section 6');
+  }
+];
+
+var sectionsLeave = [
+  // SECTION 0
+  function() {
+
+    console.log('leaving section 0');
+  },
+  // SECTION 1
+  function() {
+    TweenLite.to("#h11", 1, {opacity:0});
+    console.log('leaving section 1');
+  },
+  // SECTION 2
+  function() {
+    TweenLite.to("#roundy1", 1, {opacity:0});
+    console.log('leaving section 2');
+  },
+  // SECTION 3
+  function() {
+    TweenLite.to("#cont1", 1, {marginLeft:2*windowWidth+'px'});
+    console.log('leaving section 3');
+  },
+  // SECTION 4
+  function() {
+    TweenLite.to("#cont2", 1, {marginRight:2*windowWidth+'px'});
+    console.log('leaving section 4');
+  },
+  // SECTION 5
+  function() {
+    TweenLite.to("#cont3", 0.8, {opacity:0});
+    console.log('leaving section 5');
+  },
+  // SECTION 6
+  function() {
+    TweenLite.to("#cont4", 1, {height:0, opacity:0});
+    console.log('leaving section 6');
+  }
+];
