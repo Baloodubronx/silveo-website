a2cfb850a7d18304fc330c36238978fe702cb730
baloodubronx <julien.autebert@gmail.com>
2015-09-27 21:30:05 +0200

    new version

--- js/silveo.js
+++ js/silveo.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-
+$(document).ready(function(){
+  $('.intro').slick({
+    autoplay : true,
+    autoplaySpeed : 5000,
+    accessibility : false,
+    dots: false,
+    pauseOnHover : false,
+    //appendArrows : '.oneslide',
+    //centerMode: true,
+    //centerPadding: '200px',
+    //slideToShow : 3
+    fade : true
+  });
 });
